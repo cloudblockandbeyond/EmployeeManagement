5e22b897188a9e870fad73269369f9bc5ff15b6c
cloudblockandbeyond <cloudblockandbeyond@gmail.com>
2021-12-15 00:47:34 +0800

    Add EmployeeRepository service

---
+++ Backend.Api/Services/EmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Backend.Api.Models;
+
+namespace Backend.Api.Services
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly List<Employee> _employees;
+
+        public EmployeeRepository()
+        {
+            this._employees = new List<Employee>()
+            {
+                new Employee() { Id = new Guid(), Name = "Pavan", Department = Dept.IT, Email = "pavan@employeemanagement.com" },
+                new Employee() { Id = new Guid(), Name = "Kumar", Department = Dept.Admin, Email = "kumar@employeemanagement.com" },
+                new Employee() { Id = new Guid(), Name = "Singh", Department = Dept.HR, Email = "singh@employeemanagement.com" }
+            };
+        }
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return this._employees;
+        }
+    }
+}
---
+++ Backend.Api/Services/IEmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Backend.Api.Models;
+
+namespace Backend.Api.Services
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetEmployees();
+    }
+}
